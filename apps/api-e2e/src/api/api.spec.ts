511766b195e850c427114ef1d39310a597143e91
codingspiderfox <codingspiderfox@gmail.com>
2023-03-02 16:39:19 +0000

    fix: finally fixed the problem ...

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,6 +1,7 @@
+import { DEFAULT_DB_NAME } from 'apps/api/src/app/app.module';
 import { MyData, MySchema } from 'apps/api/src/app/schema';
 import axios from 'axios';
-import { connect, model } from 'mongoose';
+import { connect, connection } from 'mongoose';
 import { DB_URI, resetDb, sleep } from '../support/helpers';
 
 describe('Post twice, then get', () => {
@@ -13,16 +14,18 @@ describe('Post twice, then get', () => {
   it('should return two messages', async () => {
     const deleteResult = await axios.delete('/');
     expect(deleteResult.status).toBe(200);
+
     const post1 = await axios.post('/', { message: 'Hello' });
     expect(post1.status).toBe(201);
     const post2 = await axios.post('/', { message: 'Hello2' });
     expect(post2.status).toBe(201);
 
     const res = await axios.get(`/`);
-
     expect(res.status).toBe(200);
+
     await connect(DB_URI);
-    const myModel = model<MyData>('MyData', MySchema);
+    const apiDb = connection.useDb(DEFAULT_DB_NAME);
+    const myModel = apiDb.model<MyData>('MyData', MySchema);
     const documents = await myModel.find({});
     expect(documents.length).toBe(2);
   });
